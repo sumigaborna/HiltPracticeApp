b5021e6c1d19839a6731dd473e4ce0ca0c7371bf
Borna <sumiga.borna@gmail.com>
2021-03-11 10:21:24 +0100

    Done with video 2.

--- app/src/main/java/com/sumigaborna/hiltpracticeapp/MainActivity.kt
+++ app/src/main/java/com/sumigaborna/hiltpracticeapp/MainActivity.kt
@@ -2,11 +2,29 @@ package com.sumigaborna.hiltpracticeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(someClass.doAThing())
+        println(someClass.doSomeOtherThing())
     }
+}
+
+class SomeClass @Inject constructor(private val someOtherClass: SomeOtherClass){
+    fun doAThing():String = "Look I did a thing"
+    fun doSomeOtherThing():String = someOtherClass.doSomeOtherThing()
+}
+
+class SomeOtherClass @Inject constructor(){
+    fun doSomeOtherThing() : String = "Look this is some other thing"
 }

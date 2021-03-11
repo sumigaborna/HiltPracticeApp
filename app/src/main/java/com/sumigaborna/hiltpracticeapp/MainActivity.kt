b56adeb70158f2402da78fc15dbc35d3e834bd0e
Borna <sumiga.borna@gmail.com>
2021-03-11 14:46:26 +0100

    Done with video 5.

--- app/src/main/java/com/sumigaborna/hiltpracticeapp/MainActivity.kt
+++ app/src/main/java/com/sumigaborna/hiltpracticeapp/MainActivity.kt
@@ -2,12 +2,14 @@ package com.sumigaborna.hiltpracticeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
-import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
-import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -18,20 +20,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         println(someClass.doAThing())
     }
 }
 
-@AndroidEntryPoint
-class MyFragment : Fragment(){
-
-    @Inject
-    lateinit var someClass: SomeClass
-
+class SomeClass
+@Inject
+constructor(
+        private val someInterfaceImpl: SomeInterface,
+        private val gson: Gson
+) {
+    fun doAThing(): String {
+        return "Look I got: ${someInterfaceImpl.getAThing()}"
+    }
 }
 
-//@FragmentScoped //this won't run because fragment scope is smaller than @ActivityScoped
-@ActivityScoped
-class SomeClass @Inject constructor() {
-    fun doAThing(): String = "Look I did a thing"
+class SomeInterfaceImpl @Inject constructor() : SomeInterface {
+    override fun getAThing(): String {
+        return "A Thing"
+    }
+}
+
+interface SomeInterface {
+    fun getAThing(): String
+}
+
+@InstallIn(ActivityComponent::class)
+@Module
+class MyModule {
+    @ActivityScoped
+    @Provides
+    fun bindSomeDependency(someInterfaceImpl: SomeInterfaceImpl): SomeInterface = someInterfaceImpl
+
+    @ActivityScoped
+    @Provides
+    fun bindGson(): Gson {
+        return Gson()
+    }
 }

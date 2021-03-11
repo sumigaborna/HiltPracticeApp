00ad851f0b0a7fa923d556bca64be60b45f72936
Borna <sumiga.borna@gmail.com>
2021-03-11 09:29:02 +0100

    Init

---
+++ app/src/main/java/com/sumigaborna/hiltpracticeapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sumigaborna.hiltpracticeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.HiltAndroidApp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
